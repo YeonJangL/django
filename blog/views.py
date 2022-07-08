fee9f1b8aeafe1bea997d59fc6c836b9d61f9a04
YeonJang <lej29331814@gmail.com>
2022-07-08 14:47:35 +0900

    포스트를 작성할 때 텍스트로 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -138,26 +138,3 @@ def tag_page(request, slug):
             'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
-
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
-#
-#     return render(
-#         request,
-#         'blog/index.html',
-#         {
-#             'posts': posts,
-#         }
-#     )
-
-
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
-#
-#     return render(
-#         request,
-#         'blog/single_post_page.html',
-#         {
-#             'post': post,
-#         }
-#     )
