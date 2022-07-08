e52114cb045539812b04c5d5615619ccfa851aab
YeonJang <lej29331814@gmail.com>
2022-07-08 14:40:58 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,11 @@ from .models import Post, Category, Tag
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name', )}
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -12,6 +13,7 @@ class Tag(models.Model):
     def get_absolute_url(self):
         return f'/blog/tag/{self.slug}/'
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -21,6 +23,10 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/category/{self.slug}/'
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -36,7 +42,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-
     tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
